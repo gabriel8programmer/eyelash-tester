39b76e61aa368c8872327a1b83846a5077e224f4
gabriel8programmer <gabriel8github@gmail.com>
2024-11-06 23:53:06 -0300

    creating system what to identify a possible user face

--- src/components/Camera/index.js
+++ src/components/Camera/index.js
@@ -1,48 +1,57 @@
+import { Camera, CameraType } from 'expo-camera/legacy';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as FaceDetector from "expo-face-detector"
 
-import { useEffect, useState } from "react"
-import { View, Text, StyleSheet } from "react-native"
-import { Camera, useCameraDevice, useCameraPermission, useFrameProcessor } from "react-native-vision-camera"
-
-//style
 import styles from "./styles"
 
-export default () => {
+const handleFacesDetected = ({ faces }) => {
+    console.log(faces);
+};
 
-    const device = useCameraDevice("front")
-    const { hasPermission, requestPermission } = useCameraPermission()
-    const [permission, setPermission] = useState(false)
-
-    //frameprocessor 
-    const frameProcessor = useFrameProcessor((frame) => {
-        'worklet'
-        console.log(`Frame: ${frame.width}x${frame.height} (${frame.pixelFormat})`)
-    }, [])
-
-    useEffect(() => {
-        (async () => {
-            const status = await requestPermission()
-            setPermission(status)
-        })()
-    }, [])
+export default function App() {
+    const [type, setType] = useState(CameraType.back);
+    const [permission, requestPermission] = Camera.useCameraPermissions();
 
     if (!permission) {
-        <View style={styles.container}>
-            <Text>Permissão para usar a câmera é necessária.</Text>
-        </View>
+        // Camera permissions are still loading
+        return <View />;
     }
 
-    if (!device) {
-        <View style={styles.container}>
-            <Text>Dispositivo de câmera não encontrado.</Text>
-        </View>
+    if (!permission.granted) {
+        // Camera permissions are not granted yet
+        return (
+            <View style={styles.container}>
+                <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+                <Button onPress={requestPermission} title="grant permission" />
+            </View>
+        );
+    }
+
+    function toggleCameraType() {
+        setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
     }
 
     return (
-        <Camera
-            style={StyleSheet.absoluteFill}
-            device={device}
-            isActive={true}
-            frameProcessor={frameProcessor}
-        />
-    )
+        <View style={styles.container}>
+            <Camera
+                style={styles.camera}
+                type={type}
+                onFacesDetected={handleFacesDetected}
+                faceDetectorSettings={{
+                    mode: FaceDetector.FaceDetectorMode.fast,
+                    detectLandmarks: FaceDetector.FaceDetectorLandmarks.none,
+                    runClassifications: FaceDetector.FaceDetectorClassifications.none,
+                    minDetectionInterval: 100,
+                    tracking: true,
+                }}
+            >
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+                        <Text style={styles.text}>Flip Camera</Text>
+                    </TouchableOpacity>
+                </View>
+            </Camera>
+        </View>
+    );
 }
--- src/components/Camera/styles.js
+++ src/components/Camera/styles.js
@@ -4,7 +4,31 @@ import { StyleSheet } from "react-native"
 export default StyleSheet.create({
     container: {
         flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    message: {
+        textAlign: 'center',
+        paddingBottom: 10,
+    },
+    camera: {
+        flex: 1,
+        width: '100%'
+    },
+    buttonContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: 'transparent',
+        margin: 64,
+    },
+    button: {
+        flex: 1,
+        alignSelf: 'flex-end',
         alignItems: 'center',
-        justifyContent: 'center'
-    }
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
+    },
 })
